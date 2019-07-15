0ad3549f1f789f7a80cb94d9f1496f651b5d569f
RafSwiggers <rafswiggers@gmail.com>
2019-07-15 11:23:39 +0200

    added weight and height

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ const search = document.getElementById('search')
 const button = document.getElementById('button')
 const sprite = document.getElementById('sprite')
 const imgDiv = document.getElementById("imDiv")
+const field = document.getElementsByClassName('field')
 
 function loadpokemon() {
     document.addEventListener("load", async function(e) {
@@ -17,10 +18,14 @@ function pokedex() {
         let pokemon = search.value;
         let api = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
         let response = await axios.get(api);
-        console.log(response)
-        console.log()
+        console.log(response.data)
+        field[0].insertAdjacentText("beforeend", response.data.name)
+        field[1].insertAdjacentText("beforeend", response.data.height)
+        field[2].insertAdjacentText("beforeend", response.data.weight)
+        sprite.src = response.data.sprites.front_default;
         imgDiv.style.display = "block";
-        sprite.src = response.data.sprites.front_default
+
+
     })
 }
 pokedex();
