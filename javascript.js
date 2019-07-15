9e347878a95c9ffcbb21a771043d90ae9f3a0bb3
RafSwiggers <rafswiggers@gmail.com>
2019-07-15 11:58:32 +0200

    add the stats

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
-const search = document.getElementById('search')
-const button = document.getElementById('button')
-const sprite = document.getElementById('sprite')
-const imgDiv = document.getElementById("imDiv")
-const field = document.getElementsByClassName('field')
+const search = document.getElementById('search');
+const button = document.getElementById('button');
+const sprite = document.getElementById('sprite');
+const imgDiv = document.getElementById("imDiv");
+const field = document.getElementsByClassName('field');
+const statName = document.getElementsByClassName('statName');
+const statField = document.getElementsByClassName('statField')
 
 function loadpokemon() {
     document.addEventListener("load", async function(e) {
@@ -19,11 +21,18 @@ function pokedex() {
         let pokemon = result.toLowerCase();
         let api = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
         let response = await axios.get(api);
-        console.log(response.data)
+        console.log(response.data);
         field[0].innerHTML = response.data.name;
         field[1].innerHTML = response.data.height;
         field[2].innerHTML = response.data.weight;
+        statName[0].innerHTML = response.data.stats[5].stat.name + ": ";
+        statField[0].innerHTML = response.data.stats[5].base_stat;
+        statName[1].innerHTML = response.data.stats[3].stat.name + ": ";
+        statField[1].innerHTML = response.data.stats[3].base_stat;
+        statName[2].innerHTML = response.data.stats[0].stat.name + ": ";
+        statField[2].innerHTML = response.data.stats[0].base_stat;
         sprite.src = response.data.sprites.front_default;
+
         imgDiv.style.display = "block";
 
 
