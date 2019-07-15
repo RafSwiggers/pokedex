21915869995af726b2d6b5984bca219de790e2ec
RafSwiggers <rafswiggers@gmail.com>
2019-07-15 16:38:35 +0200

    added previous evolution function

--- javascript.js
+++ javascript.js
@@ -4,8 +4,9 @@ const sprite = document.getElementById('sprite');
 const imgDiv = document.getElementById("imDiv");
 const field = document.getElementsByClassName('field');
 const statName = document.getElementsByClassName('statName');
-const statField = document.getElementsByClassName('statField')
-const moves = document.getElementsByClassName("move")
+const statField = document.getElementsByClassName('statField');
+const moves = document.getElementsByClassName("move");
+const evolve = document.getElementsByClassName('evolve');
 
 function loadpokemon() {
     document.addEventListener("load", async function(e) {
@@ -23,7 +24,7 @@ function pokedex() {
         let api = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
         let response = await axios.get(api);
         let pokeid = response.data.id;
-        let evoapi = `https://pokeapi.co/api/v2/evolution-chain/${pokeid}`;
+        let evoapi = `https://pokeapi.co/api/v2/pokemon-species/${pokeid}`;
         let evoresponse = await axios.get(evoapi);
         console.log(evoresponse.data);
         field[0].innerHTML = response.data.id;
@@ -40,10 +41,25 @@ function pokedex() {
         moves[1].innerHTML = response.data.moves[1].move.name;
         moves[2].innerHTML = response.data.moves[2].move.name;
         moves[3].innerHTML = response.data.moves[3].move.name;
-
-
-
         sprite.src = response.data.sprites.front_default;
+
+
+
+        if (evoresponse.data.evolves_from_species) {
+            evolve[0].style.display = "block";
+            evolve[1].style.display = "block";
+            evolve[2].style.display = "block";
+            let evoname = evoresponse.data.evolves_from_species.name;
+            let evonameapi = `https://pokeapi.co/api/v2/pokemon/${evoname}`;
+            let evonameresponse = await axios.get(evonameapi);
+            evolve[1].innerHTML = evonameresponse.data.name;
+            evolve[2].src = evonameresponse.data.sprites.front_default;
+
+        } else {
+            evolve[0].style.display = "none";
+            evolve[1].style.display = "none";
+            evolve[2].style.display = "none";
+        }
 
         imgDiv.style.display = "block";
 
